9374ad47d0cf76279d949176732bf5df7e08198b
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2017-03-03 11:20:14 -0600

    gitignore is working for .idea/*

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,6 @@
 import {expect} from 'chai';
 import Index from '../src/index';
 
-//j Di
 describe('Index', () => {
     it('should return `Hello World`', () => {
         expect("Hello World").to.equal(Index());
