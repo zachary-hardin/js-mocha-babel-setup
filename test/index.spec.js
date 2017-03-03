e49d0438be9577b67962fa9401908993ee9561fa
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2017-03-03 10:35:32 -0600

    testing .gitignore

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import Index from '../src/index';
 
+// Test
 describe('Index', () => {
     it('should return `Hello World`', () => {
         expect("Hello World").to.equal(Index());
