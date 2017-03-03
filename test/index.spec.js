9b298f58642fcf54ee7a6c8264cd165b9b7c2657
Zachary Hardin <zacharyhardin@Zacharys-MacBook-Pro-4.local>
2017-03-03 11:17:23 -0600

    gitignore not working

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,6 @@
 import {expect} from 'chai';
 import Index from '../src/index';
 
-// Test
 describe('Index', () => {
     it('should return `Hello World`', () => {
         expect("Hello World").to.equal(Index());
